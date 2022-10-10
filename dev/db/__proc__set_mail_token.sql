468ffd91e5c7dd48071515fff3725cd977ec9c9c
koko <github@koko-campus.com>
2022-10-10 11:30:29 +0000

    ストアド作った。

--- dev/db/__proc__set_mail_token.sql
+++ dev/db/__proc__set_mail_token.sql
@@ -1,6 +1,7 @@
 
 CREATE PROCEDURE set_mail_token
 @pre_user_id VARCHAR(254)
+@token VARCHAR(32)
 AS
 BEGIN
 
@@ -15,12 +16,12 @@ WHERE EXISTS(
 	WHERE pre_user_id = @pre_user_id
 );
 
-INSERT INTO pre_users(pre_user_id)
-SELECT @pre_user_id
+INSERT INTO pre_users(pre_user_id, token)
+SELECT @pre_user_id, @token
 WHERE @is_exist <> 0;
 
 UPDATE pre_users
-SET updt = CURRENT_TIMESTAMP
+SET token = @token, updt = CURRENT_TIMESTAMP
 WHERE @is_exist = 1;
 
 
