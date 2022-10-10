a5c2f4af65b768c80cc58bb7a54198347aa4ff48
koko <github@koko-campus.com>
2022-10-10 15:42:45 +0000

    AIを0から始める。

--- dev/db/quiz_templates.sql
+++ dev/db/quiz_templates.sql
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE quiz_templates(
-	quiztemplate_id INT PRIMARY KEY IDENTITY(1, 1),
+	quiztemplate_id INT PRIMARY KEY IDENTITY(0, 1),
 	owning_user VARCHAR(254) NULL,
 	owning_session VARCHAR(32) NOT NULL,
 	is_public BIT NOT NULL,
