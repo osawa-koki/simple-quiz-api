207cd456639f71bef8e723026f276ea9b97d4b86
koko <github@koko-campus.com>
2022-10-10 06:21:48 +0000

    サインアップメール送信処理。

--- fn/MailClient.cs
+++ fn/MailClient.cs
@@ -1,5 +1,6 @@
 using System.Net.Mail;
 using System.Text;
+using System.Net;
 
 namespace MailMod
 {
@@ -32,7 +33,7 @@ namespace MailMod
 			SMTPServer = smtpServer;
 		}
 
-		internal bool Send(MailSetting mailSetting)
+		internal static bool Send(MailSetting mailSetting)
 		{
 			if (SMTPServer == null)
 			{
@@ -41,26 +42,29 @@ namespace MailMod
 			}
 
 			MailMessage message = new(mailSetting.MailFrom, mailSetting.MailTo);
-			foreach (var CC in mailSetting.MailCC)
-			{
-				message.CC.Add(CC);
-			}
-			foreach (var BCC in mailSetting.MailBCC)
-			{
-				message.Bcc.Add(BCC);
-			}
+			// foreach (var CC in mailSetting.MailCC)
+			// {
+			// 	message.CC.Add(CC);
+			// }
+			// foreach (var BCC in mailSetting.MailBCC)
+			// {
+			// 	message.Bcc.Add(BCC);
+			// }
 
 			message.Subject = mailSetting.Subject;
 			message.Body = mailSetting.Body;
 			message.IsBodyHtml = mailSetting.IsHTML;
 
+
 			try
 			{
 				using var client = new SmtpClient(SMTPServer);
+				client.Credentials = new NetworkCredential(Env.SMTPSERVER_USER, Env.SMTPSERVER_PASSWORD);
+				client.Timeout = 10 * 1000;
 				client.Send(message);
 				return true;
 			}
-			catch (System.Exception)
+			catch (Exception ex)
 			{
 				return false;
 			}
