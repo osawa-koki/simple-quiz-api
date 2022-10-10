479dd5ff3ed82264b4715cc01c2e6b45bf378c6c
koko <github@koko-campus.com>
2022-10-10 13:55:21 +0000

    ハッシュ化関数の作成。

--- fn/Util.cs
+++ fn/Util.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 
 
 internal static class Util
@@ -5,6 +6,11 @@ internal static class Util
 	internal static string MergeUri(string a, string b)
 	{
 		return new Uri(new Uri(a), b).AbsolutePath;
+	}
+
+	internal static string? HashPassword(string target)
+	{
+		return SHA512.Create(target)?.ToString();
 	}
 
 
