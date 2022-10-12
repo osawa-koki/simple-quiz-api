f4ca21bebc04f9a9ff1ff017a0f41c22b64692f7
koko <github@koko-campus.com>
2022-10-12 12:20:05 +0000

    ルームキーワード作った。

--- dev/db/quiz_template_keywords.sql
+++ dev/db/quiz_template_keywords.sql
@@ -3,7 +3,7 @@
 CREATE TABLE quiz_template_keywords(
 	quiztemplate_id INT,
 	keyword NVARCHAR(30),
-	CONSTRAINT fgk_tmpl_kwd FOREIGN KEY(quiztemplate_id) REFERENCES quiz_templates(quiztemplate_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fgk_tmpl_kwd FOREIGN KEY(quiztemplate_id) REFERENCES quiz_templates(quiztemplate_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
