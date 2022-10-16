bfb04f594a6951da26dea82740bba3a2abd60d68
osawakoki <osawakoki@github.com>
2022-10-16 10:45:34 +0000

    varcharで統一したほうが楽だ。

--- dev/db/table/quiz_template_keywords.sql
+++ dev/db/table/quiz_template_keywords.sql
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE quiz_template_keywords(
-	quiztemplate_id CHAR(32),
+	quiztemplate_id VARCHAR(32),
 	keyword NVARCHAR(30),
 	CONSTRAINT fgk_tmpl_kwd FOREIGN KEY(quiztemplate_id) REFERENCES quiz_templates(quiztemplate_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
