b7a2ce83cdf53c84cc8da6459b0e188056a0a950
osawakoki <osawakoki@github.com>
2022-10-16 09:53:50 +0000

    テンプレートのイイネカラムを用意したよ♪

--- dev/db/table/quiz_templates.sql
+++ dev/db/table/quiz_templates.sql
@@ -6,6 +6,9 @@ CREATE TABLE quiz_templates(
 	owning_session VARCHAR(32) NOT NULL,
 	is_public BIT NOT NULL,
 	content VARCHAR(300) NOT NULL,
+	n_of_used INT DEFAULT 0,
+	n_of_liked INT DEFAULT 0,
+	n_of_disliked INT DEFAULT 0,
 	rgdt DATETIME DEFAULT dbo.GET_TOKYO_DATETIME(),
 	updt DATETIME DEFAULT dbo.GET_TOKYO_DATETIME()
 );
