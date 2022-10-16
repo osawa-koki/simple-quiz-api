f71d288a9b9b0db6dee793d7e69ea0a6b62a3141
osawakoki <osawakoki@github.com>
2022-10-16 06:32:04 +0000

    文字化け回避した。

--- dev/db/table/rooms.sql
+++ dev/db/table/rooms.sql
@@ -4,7 +4,7 @@ CREATE TABLE rooms(
 	room_id VARCHAR(32) PRIMARY KEY,
 	room_name VARCHAR(30) NOT NULL,
 	room_icon VARCHAR(38) NULL,
-	explanation VARCHAR(100) NULL,
+	explanation NVARCHAR(100) NULL,
 	pw CHAR(4) NULL CHECK(LEN(pw) = 4),
 	is_public BIT NOT NULL,
 	is_valid BIT DEFAULT 1,
