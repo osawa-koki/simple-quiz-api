fc10a69ae5195c69c131be05a8ab949d02d6d631
osawakoki <osawakoki@github.com>
2022-10-16 14:17:30 +0000

    クイズテーブルの構成変えた。

--- dev/db/table/quizzes.sql
+++ dev/db/table/quizzes.sql
@@ -3,6 +3,8 @@
 CREATE TABLE quizzes(
 	quiz_id VARCHAR(32) PRIMARY KEY,
 	room_id VARCHAR(32),
+	sequential INT NOT NULL,
+	content NVARCHAR(100) NOT NULL,
 	INDEX idx_belonging(room_id),
 	CONSTRAINT fgk_belonging FOREIGN KEY(room_id) REFERENCES rooms(room_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
