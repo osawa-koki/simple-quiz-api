55916820ef094229a03d093addfd388e0cc5b3c9
osawakoki <osawakoki@github.com>
2022-10-16 15:03:29 +0000

    ルームメンバー管理方法で迷った。

--- dev/db/table/rooms.sql
+++ dev/db/table/rooms.sql
@@ -8,6 +8,8 @@ CREATE TABLE rooms(
 	pw CHAR(4) NULL CHECK(LEN(pw) = 4),
 	is_public BIT NOT NULL,
 	is_valid BIT DEFAULT 1,
+	owning_user VARCHAR(16) NULL,
+	owning_session VARCHAR(32) NULL,
 	rgdt DATETIME DEFAULT dbo.GET_TOKYO_DATETIME(),
 	updt DATETIME DEFAULT dbo.GET_TOKYO_DATETIME()
 );
