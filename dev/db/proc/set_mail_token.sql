e70154e1199d8e988c8b085332510f4e06e847c5
osawakoki <osawakoki@github.com>
2022-10-17 09:37:03 +0000

    ストアド変えた。

--- dev/db/proc/set_mail_token.sql
+++ dev/db/proc/set_mail_token.sql
@@ -2,7 +2,12 @@
 
 CREATE PROCEDURE set_mail_token
 @mail VARCHAR(254),
-@token VARCHAR(32)
+@token VARCHAR(32),
+@user_id VARCHAR(32),
+@user_name NVARCHAR(25),
+@pw VARCHAR(512),
+@comment NVARCHAR(300),
+@user_icon VARCHAR(38),
 AS
 BEGIN
 
@@ -15,12 +20,19 @@ WHERE EXISTS(
 	WHERE mail = @mail
 );
 
-INSERT INTO pre_users(mail, token)
-SELECT @mail, @token
+INSERT INTO pre_users(token, mail, user_id, user_name, pw, comment, user_icon)
+SELECT @token, @mail, @user_id, @user_name, @pw, @comment, @user_icon
 WHERE @is_exist = 0;
 
 UPDATE pre_users
-SET token = @token, updt = CURRENT_TIMESTAMP
+SET
+	token = @token,
+	user_id = @user_id,
+	user_name = @user_name,
+	pw = @pw,
+	comment = @comment,
+	user_icon = @user_icon,
+	updt = dbo.GET_TOKYO_DATETIME()
 WHERE mail = @mail AND @is_exist = 1;
 
 
